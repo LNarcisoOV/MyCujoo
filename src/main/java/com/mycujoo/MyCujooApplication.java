4ff7c4a6f22a84f5d0360afcb648f27ed94cf408
Leonardo Narciso Oliveira do Vale <leonardo.vale@marquise.com.br>
2019-03-12 16:43:30 -0300

    Just a test for jenkis integration.

--- src/main/java/com/mycujoo/MyCujooApplication.java
+++ src/main/java/com/mycujoo/MyCujooApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyCujooApplication{
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyCujooApplication.class, args);
 	}
